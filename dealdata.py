6f3b1911645cca1fa227076cc41f93a98649fa46
zhengyi1212 <zhuzhengyi2013@126.com>
2018-09-25 21:40:01 +0800

    update

--- dealdata.py
+++ dealdata.py
@@ -1,34 +1,60 @@
 #encoding=utf-8
 
-from keras.preprocessing.image import ImageDataGenerator,array_to_img,img_to_array,load_img
-import numpy as np
+from os import listdir
+from shutil import copyfile
+# from keras.preprocessing.image import ImageDataGenerator,array_to_img,img_to_array,load_img
+# import numpy as np
+import cv2
 
-#图片生成器
-datagen = ImageDataGenerator(
-    rotation_range=40,      #随机旋转度数
-    width_shift_range=0.2,  #随机水平平移
-    height_shift_range=0.2, #随机竖直平移
-    rescale=1/255,          #数据归一化
-    shear_range=0.2,        #随机裁剪
-    zoom_range=0.2,         #随机放大
-    horizontal_flip=True,   #水平翻转
-    fill_mode='nearest',     #填充方式
-)
+def splitfile(trainpath,catpath,dogpath):
+    allfile = listdir(trainpath)
+    for filename in allfile:
+        # print(filename)
+        # break
+        if filename[0]=='c':
+            copyfile(trainpath+filename,catpath+filename)
+        if filename[0]=='d':
+            copyfile(trainpath + filename, dogpath + filename)
+    pass
 
-test_datagen = ImageDataGenerator(
-    rescale=1/255
-)
+def imgGen_test():
+    # 图片生成器
+    datagen = ImageDataGenerator(
+        rotation_range=40,  # 随机旋转度数
+        width_shift_range=0.2,  # 随机水平平移
+        height_shift_range=0.2,  # 随机竖直平移
+        rescale=1 / 255,  # 数据归一化
+        shear_range=0.2,  # 随机裁剪
+        zoom_range=0.2,  # 随机放大
+        horizontal_flip=True,  # 水平翻转
+        fill_mode='nearest',  # 填充方式
+    )
 
+    test_datagen = ImageDataGenerator(
+        rescale=1 / 255
+    )
 
-img = None  #读入一张图片
+    img = load_img(imgPath)  # 读入一张图片
+    x = img_to_array(img)
+    x = np.expand_dims(x, 0)
+    i = 0
+    for batch in datagen.flow(x, batch_size=1, save_to_dir=savePath + 'temp', save_prefix='new_cate',
+                              save_format='jpg'):
+        i += 1
+        if i == 20:
+            break
 
-x = img_to_array(img)
+if __name__ == '__main__':
+    trainpath = '/home/zzy/TrainData/kaggle_cat_dog/train/'
+    testpath = '/home/zzy/TrainData/kaggle_cat_dog/test1/'
+    catpath = '/home/zzy/TrainData/kaggle_cat_dog/cat/'
+    dogpath = '/home/zzy/TrainData/kaggle_cat_dog/dog/'
 
-x = np.expand_dims(x,0)
+    imgPath = '/home/zzy/TrainData/kaggle_cat_dog/test1/34.jpg'
+    savePath = '/home/zzy/TrainData/kaggle_cat_dog/'
 
-i= 0
-for batch in datagen.flow(x,batch_size=1,save_to_dir='temp',save_prefix='new_cate',save_format='jpg'):
-    i+=1
-    if i==20:
-        break
+    # splitfile(trainpath,catpath,dogpath)
+
+    image = cv2.imread(trainpath+'cat.4567.jpg')
+    print(image.shape)
 
--- testvgg16.py
+++ testvgg16.py
@@ -8,11 +8,11 @@ import numpy as np
 label = np.array(['cat','dog'])
 model = load_model('vgg16catVSdog.h5')
 
-image = load_img('./image/test/cat/cat000.jpg')
+image = load_img('/home/zzy/TrainData/test1/1.jpg')
 
 image = image.resize((150,150))
 image = img_to_array(image)
 image = image/255
 image = np.expand_dims(image,0)
 
-print label[model.predict_classes(image)]
+print(label[model.predict_classes(image)])
